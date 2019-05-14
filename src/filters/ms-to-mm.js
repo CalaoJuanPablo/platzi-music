76dea2175fd4c0eb26ec9eec75fb296a7db24424
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-05-14 08:05:10 -0500

    filters

---
+++ src/filters/ms-to-mm.js
@@ -0,0 +1,21 @@
+const msToMm = {};
+
+function convertMsToMm(ms) {
+  function leftPad(number) {
+    const pad = '00';
+    return pad.substring(0, pad.length - number.length) + number;
+  }
+
+  const min = Math.floor(ms / 60000);
+  const sec = ((ms % 60000) / 1000).toFixed(0);
+
+  return `${min}:${leftPad(sec.toString())}`
+}
+
+msToMm.install = function(Vue) {
+  Vue.filter('ms-to-mm', (val) => {
+    return convertMsToMm(val)
+  })
+}
+
+export default msToMm
--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import EventBus from './plugins/event-bus'
 import routes from './routes'
+import msToMm from './filters/ms-to-mm'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 Vue.use(EventBus)
+Vue.use(msToMm)
 
 const router = new VueRouter({
   routes,
