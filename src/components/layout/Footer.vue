a974d164bb9d38530816efa667cdacfe71e62951
Juan Pablo Calao Perez <juan.calao@ideaware.co>
2019-06-10 15:50:23 -0500

    Footer twitter link

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,5 +2,6 @@
   footer.footer
     .container
       .content.has-text-centered
-        p 💻 with ❤ by @CalaoJuanPablo
+        p 💻 with ❤ by
+          a(href="https://twitter.com/CalaoJuanPablo" target="_blank") &nbsp;@CalaoJuanPablo
 </template>
