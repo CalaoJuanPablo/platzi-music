7306ffc520573f8257d0a77e22ca225a8273eda6
Juan Pablo Calao <jcalaop@gmail.com>
2020-08-11 22:43:53 -0500

    fix vulneravilities

--- src/services/track.js
+++ src/services/track.js
@@ -5,18 +5,18 @@ const trackService = {}
 trackService.search = function(q) {
   const type = 'track'
 
-  return platziMusicService.get('/search', {
-    params: {
-      q,
-      type
-    }
-  })
+  return platziMusicService
+    .get('/search', {
+      params: {
+        q,
+        type
+      }
+    })
     .then(res => res.data)
 }
 
 trackService.getById = function(id) {
-  return platziMusicService.get(`/tracks/${id}`)
-    then(res => res.data)
+  return platziMusicService.get(`/tracks/${id}`).then(res => res.data)
 }
 
 export default trackService
